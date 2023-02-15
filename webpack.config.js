5972186007bc464dc4332de325127b040d76d8d3
Карина Фарадян <faradiankarina@gmail.com>
2023-02-15 17:28:35 +0400

    build: create index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'production',
     entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'build')
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'build'),
+        clean: true,
     }
 }

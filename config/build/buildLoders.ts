8da1e2276ec926a4c9bb954e8d85418262ba4b24
Карина Фарадян <faradiankarina@gmail.com>
2023-03-07 14:27:23 +0400

    build: trying babel plugin

--- config/build/buildLoders.ts
+++ config/build/buildLoders.ts
@@ -8,6 +8,28 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     use: ['@svgr/webpack'],
   };
 
+  // const babelLoader = {
+  //   test: /\.(js|jsx|ts|tsx)$/,
+  //   exclude: /node_modules/,
+  //   use: {
+  //     loader: 'babel-loader',
+  //     options: {
+  //       presets: ['@babel/preset-env'],
+  //       plugins: [
+  //         [
+  //           'i18next-extract',
+  //           {
+  //             locales: ['en', 'ru'],
+  //             keyAsDefaultValue: false,
+  //             saveMissing: true,
+  //             outputPath: 'public/locales/{{locale}}/{{ns}}.json',
+  //           },
+  //         ],
+  //       ],
+  //     },
+  //   },
+  // };
+
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
